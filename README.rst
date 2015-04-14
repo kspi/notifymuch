a7d4a111c09326c09fe92aeb2d31167bc3993eb3
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-04-14 19:53:56 +0300

    Add README and update setup.py

---
+++ README.rst
@@ -0,0 +1,9 @@
+notifymuch
+==========
+
+This is a simple program that displays desktop notifications for unread
+mail (or actually any query) in the notmuch database. The notification
+can optionally have a button to run a mail client.
+
+Configuration is stored in ``~/.config/notifymuch/notifymuch.cfg``,
+which is created on first run.
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+
+def readme():
+    with open("README.rst", "r") as f:
+        return f.read()
+
+
 setup(
     name="notifymuch",
     version="0.1",
-    description="",
-    long_description="",
+    description="Display desktop notifications for unread mail in notmuch "
+                "database",
+    long_description=readme(),
     author="Kiprianas Spiridonovas",
     author_email="k.spiridonovas@gmail.com",
-    url="",
-    license="MPL",
+    url="https://github.com/kspi/notifymuch",
+    license="GPL3",
     packages=['notifymuch'],
     scripts=["bin/notifymuch"],
 
