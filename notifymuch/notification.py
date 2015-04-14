4239c99491e4054bfef737a332e4d598e88096df
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-04-14 22:29:29 +0300

    Better backgrounding behaviour.

--- notifymuch/notification.py
+++ notifymuch/notification.py
@@ -1,6 +1,5 @@
-import sys
-import os
 import shlex
+import subprocess
 from gi.repository import Notify, Gio
 from notifymuch import config
 from notifymuch.messages import Messages
@@ -15,8 +14,7 @@ class NotifymuchNotification(Gio.Application):
     def __init__(self):
         Gio.Application.__init__(
                 self,
-                application_id="net.wemakethings.Notifymuch",
-                flags=Gio.ApplicationFlags.FLAGS_NONE)
+                application_id="net.wemakethings.Notifymuch")
         self.connect('startup', self.on_startup)
         self.connect('activate', self.on_activate)
 
@@ -30,14 +28,16 @@ class NotifymuchNotification(Gio.Application):
                     'mail-client',
                     'Run mail client',
                     self.action_mail_client)
-        self.notification.connect('closed', lambda e: self.release())
+        self.notification.connect('closed', lambda e: self.quit())
         self.hold()
 
     def on_activate(self, data):
         config.load()
         messages = Messages()
         summary = messages.unseen_summary()
-        if summary != "":
+        if summary == "":
+            self.release()
+        else:
             self.notification.update(
                     summary="{count} unread messages".format(
                         count=messages.count()),
@@ -46,14 +46,12 @@ class NotifymuchNotification(Gio.Application):
             self.notification.show()
 
     def action_mail_client(self, action, data):
-        self.notification.close()
-        tokens = shlex.split(config.get("mail_client"))
-        if os.fork() == 0:
-            os.execvp(tokens[0], tokens)
+        self.release()
+        subprocess.Popen(shlex.split(config.get("mail_client")))
 
 
 def show_notification():
-    # Gio.Application.run blocks in case this is the primary instance, and
-    # returns immediately otherwise. We fork here so that it never blocks.
-    if os.fork() == 0:
-        sys.exit(NotifymuchNotification().run())
+    """If a notification is open already, asks it to update itself and returns
+    immediately. Otherwise opens a notification and blocks until it is
+    closed."""
+    NotifymuchNotification().run()
